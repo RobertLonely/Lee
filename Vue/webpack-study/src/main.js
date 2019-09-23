e829140525a21328da0acd0fa76aa1b8bb9ce639
lee <2629162486@qq.com>
2019-09-23 22:58:11 +0800

    url-loader

--- Vue/webpack-study/src/main.js
+++ Vue/webpack-study/src/main.js
@@ -6,6 +6,10 @@ import './css/index.css'
 import './css/index.less'
 //导入 scss样式表
 // import './css/index.scss'
+//导入bootstrap样式表
+// 注意： 如果要通过路径的形式，去引入 node_modules 中相关的文件，可以直接省略 路径前面的 node_modules 这一层目录，直接写 包的名称，然后后面跟上具体的文件路径
+// 不写 node_modules 这一层目录 ，默认 就会去 node_modules 中查找
+import 'bootstrap/dist/css/bootstrap.css'
 
 $(function() {
   $("li:odd").css("backgroundColor", "red");
--- Vue/webpack-study/webpack.config.js
+++ Vue/webpack-study/webpack.config.js
@@ -6,33 +6,42 @@ var webpack = require("webpack");
 // 导出一个配置对象，将来webpack在启动的时候，会默认来查找webpack.config.js，并读取这个文件中导出的配置对象，来进行打包处理
 module.exports = {
   mode: "development",
-  entry: path.resolve(__dirname, "src/main.js"), //项目入口文件
+  entry: path.resolve(__dirname, "./src/main.js"), //项目入口文件
   output: {
     //配置输出选项
-    path: path.resolve(__dirname, "dist"), //配置输出路径
+    path: path.resolve(__dirname, "./dist"), //配置输出路径
     filename: "bundle.js" //配置输出文件名
   },
   plugins: [
     new htmlWebpackPlugin({
-      template: path.resolve(__dirname, "src/index.html"), //模板路径
+      template: path.resolve(__dirname, "./src/index.html"), //模板路径
       filename: "index.html" //自动生成的html文件的名称
     }),
     new webpack.HotModuleReplacementPlugin() //实例化一个热更新的模块对象
   ],
   devServer: {
     open: true,
-    port: 8000,
+    port: 7000,
     hot: true
   },
   module: {
     //用来配置第三方loader模块
-    rules: [//文件匹配规则
+    rules: [
+      //文件匹配规则
       //处理css文件规则
       { test: /\.css$/, use: ["style-loader", "css-loader"] },
       //处理less文件规则
-    { test: /\.less$/, use: ["style-loader", "css-loader","less-loader"] },
-    //处理scss文件规则
-    { test: /\.scss$/, use: ["style-loader", "css-loader","sass-loader"] }
+      { test: /\.less$/, use: ["style-loader", "css-loader", "less-loader"] },
+      //处理scss文件规则
+      { test: /\.scss$/, use: ["style-loader", "css-loader", "sass-loader"] },
+      //处理图片规则
+      {
+        test: /\.(jpg|png|gif|jpeg)$/,
+        // use: "url-loader?limit=24968&name=[hash:8]-[name].[ext]"
+        use: "url-loader?limit=7360&name=[hash:8].[name].[ext]"
+      },
+      //处理字体文件的规则
+      { test: /\.(ttf|eot|svg|woff|woff2)$/, use: "url-loader" }
     ]
   }
 };
