010552654c1d26fbbab05fc619ad6b4fbf9c6ad4
lee <2629162486@qq.com>
2019-10-12 18:07:13 +0800

    React中的绑定事件

--- React/react-base/src/index.js
+++ React/react-base/src/index.js
@@ -3,6 +3,6 @@ import ReactDOM from "react-dom";
 
 // import "@/07.class的继承";
 // import "@/09.案例：创建评论列表";
-import "@/13.案例：counter计数器";
+import "@/14.React中的绑定事件";
 
 // ReactDOM.render(myApp, document.getElementById("app"));
