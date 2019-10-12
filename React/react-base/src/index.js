9e196df765dcbbb02539afa8ea5bc53b3cb448ea
lee <2629162486@qq.com>
2019-10-12 16:49:43 +0800

    案例：counter

--- React/react-base/src/index.js
+++ React/react-base/src/index.js
@@ -3,6 +3,6 @@ import ReactDOM from "react-dom";
 
 // import "@/07.class的继承";
 // import "@/09.案例：创建评论列表";
-import "@/12.双向绑定事件";
+import "@/13.案例：counter计数器";
 
 // ReactDOM.render(myApp, document.getElementById("app"));
