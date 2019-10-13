36d0db127107d12b6e25a4f414fa18da41f8dd32
lee <2629162486@qq.com>
2019-10-13 23:01:18 +0800

    ant 初体验

--- React/react-base/src/index.js
+++ React/react-base/src/index.js
@@ -3,6 +3,6 @@ import ReactDOM from "react-dom";
 
 // import "@/07.class的继承";
 // import "@/09.案例：创建评论列表";
-import "@/16.React-router-dom路由的基本使用";
+import "@/17.Ant Design框架初体验";
 
 // ReactDOM.render(myApp, document.getElementById("app"));
