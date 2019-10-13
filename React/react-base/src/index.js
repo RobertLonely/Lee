7406a7debf3b60ad7dfa3671b78c856938f16f7f
lee <2629162486@qq.com>
2019-10-13 22:23:00 +0800

    路由的基本使用

--- React/react-base/src/index.js
+++ React/react-base/src/index.js
@@ -3,6 +3,6 @@ import ReactDOM from "react-dom";
 
 // import "@/07.class的继承";
 // import "@/09.案例：创建评论列表";
-import "@/15.父组件使用context特性";
+import "@/16.React-router-dom路由的基本使用";
 
 // ReactDOM.render(myApp, document.getElementById("app"));
